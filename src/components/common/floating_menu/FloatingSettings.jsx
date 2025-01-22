9c888738676f045510fda441e11d48782d5b16af
fivefourtwo <fabricerio18@gmail.com>
2025-01-22 21:17:38 +0100

    Base for floating menu

---
+++ src/components/common/floating_menu/FloatingSettings.jsx
@@ -0,0 +1,17 @@
+<div style={{width: '100%', height: '100%', padding: 16, background: '#FFFDF4', boxShadow: '0px 2px 6.300000190734863px rgba(199, 197, 188, 0.30)', borderRadius: 8, overflow: 'hidden', border: '1px #C7C5BC solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 16, display: 'inline-flex'}}>
+    <div style={{alignSelf: 'stretch', paddingLeft: 24, justifyContent: 'flex-start', alignItems: 'flex-start', gap: 16, display: 'inline-flex'}}>
+        <div style={{justifyContent: 'flex-start', alignItems: 'center', gap: 8, display: 'flex'}}>
+            <div style={{padding: 12, borderRadius: 55, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', gap: 8, display: 'flex'}}>
+                <div style={{width: 20, height: 20, position: 'relative'}}>
+                    <div style={{width: 11.17, height: 13.08, left: 4.42, top: 3.50, position: 'absolute', background: '#1E1E1E'}}></div>
+                </div>
+            </div>
+        </div>
+        <div style={{flex: '1 1 0', height: 24, justifyContent: 'flex-start', alignItems: 'center', gap: 16, display: 'flex'}}>
+            <div style={{flex: '1 1 0'}}><span style="color: 'black', fontSize: 16, fontFamily: 'Manrope', fontWeight: '600', wordWrap: 'break-word'">Frau Lehrerin:</span><span style="color: 'black', fontSize: 16, fontFamily: 'Manrope', fontWeight: '500', wordWrap: 'break-word'"> 3b • Ausgedachte Grundschule</span></div>
+            <div style={{width: 24, height: 24, position: 'relative'}}>
+                <div style={{width: 18, height: 14, left: 3, top: 5, position: 'absolute', background: '#1E1E1E'}}></div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/components/common/floating_menu/FloatingStatus.jsx
@@ -0,0 +1,12 @@
+<div style={{width: '100%', height: '100%', padding: 16, background: '#FFFDF4', boxShadow: '0px 2px 6.300000190734863px rgba(199, 197, 188, 0.30)', borderRadius: 8, overflow: 'hidden', border: '1px #C7C5BC solid', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', gap: 24, display: 'inline-flex'}}>
+    <div style={{alignSelf: 'stretch', paddingLeft: 24, justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 20, height: 20, position: 'relative'}}>
+            <div style={{width: 15, height: 15, left: 2.50, top: 2.50, position: 'absolute', background: '#FA4604'}}></div>
+            <div style={{width: 7.50, height: 7.50, left: 6.25, top: 6.25, position: 'absolute', background: '#FA4604'}}></div>
+        </div>
+        <div style={{color: '#1E1E1E', fontSize: 16, fontFamily: 'Manrope', fontWeight: '600', wordWrap: 'break-word'}}>Status deiner Aufgabe</div>
+        <div style={{width: 24, height: 24, position: 'relative'}}>
+            <div style={{width: 18, height: 14, left: 3, top: 5, position: 'absolute', background: '#1E1E1E'}}></div>
+        </div>
+    </div>
+</div>
